b1beea0dccb789d60a247143bf94497062890b76
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-06-18 16:21:22 -0300

    feat add chats components and finished section 5

--- src/components/Expenses/index.jsx
+++ src/components/Expenses/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Card from "../Card";
 import ExpensesFilter from "../ExpenseFilter";
+import ExpensesChart from "../ExpensesChart";
 import ExpensesList from "../ExpensesList";
 import "./styles.css";
 
@@ -21,6 +22,7 @@ const Expenses = ({ items }) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
