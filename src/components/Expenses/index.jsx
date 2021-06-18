83f747c5d17a11d13791eab448cf6f8f650d14a4
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-06-18 14:43:09 -0300

    feat: add dynamic list

--- src/components/Expenses/index.jsx
+++ src/components/Expenses/index.jsx
@@ -4,7 +4,8 @@ import ExpensesFilter from "../ExpenseFilter";
 import ExpenseItem from "../ExpenseItem";
 import "./styles.css";
 
-const Expenses = (props) => {
+const Expenses = ({ items }) => {
+  console.log(items);
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
@@ -17,26 +18,13 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
