6a98f352eeb46e24d76a285fcdfaf0c369177958
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-06-23 13:59:46 -0300

    feat: finished project 6 and section 11

--- project-06/src/Components/Cart/index.jsx
+++ project-06/src/Components/Cart/index.jsx
@@ -10,8 +10,12 @@ const Cart = ({ onClose }) => {
   const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
   const hasItems = cartContext.items.length > 0;
 
-  const cartItemRemoveHandler = () => {};
-  const cartItemAddHandler = () => {};
+  const cartItemRemoveHandler = (id) => {
+    cartContext.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartContext.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
@@ -22,7 +26,7 @@ const Cart = ({ onClose }) => {
           amount={item.amount}
           price={item.price}
           onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
--- project-06/src/Components/Layout/HeaderCartButton/index.jsx
+++ project-06/src/Components/Layout/HeaderCartButton/index.jsx
@@ -1,17 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../../store/cart-context";
 import CartIcon from "../../Cart/CartIcon";
 import styles from "./styles.module.css";
 
 const HeaderCartButton = ({ onClick }) => {
   const cartContext = useContext(CartContext);
+  const { items } = cartContext;
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
-  const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnStyles = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={btnStyles} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
