5e5a7264547ddb0602f95fc716ea21d1a711be63
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-06-30 16:22:32 -0300

    feat: using useHistory and Prompt

--- project-17/src/pages/NewQuote/index.jsx
+++ project-17/src/pages/NewQuote/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useHistory } from "react-router";
 import QuotesForm from "../../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const { push } = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    push("/quotes");
   };
 
   return <QuotesForm onAddQuote={addQuoteHandler} />;
