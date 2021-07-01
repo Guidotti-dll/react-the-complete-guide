65a166918c0fdc95c32ef36ce91e59b5c26e9012
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-01 13:21:36 -0300

    feat: deploy project 17

--- project-17/src/routes.jsx
+++ project-17/src/routes.jsx
@@ -1,25 +1,40 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import Quotes from "./pages/Quotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+// import Quotes from "./pages/Quotes";
+// import QuoteDetail from "./pages/QuoteDetail";
+// import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
+
+const Quotes = lazy(() => import("./pages/Quotes"));
+const QuoteDetail = lazy(() => import("./pages/QuoteDetail"));
+const NewQuote = lazy(() => import("./pages/NewQuote"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route component={Quotes} path="/quotes" exact />
-          <Route component={QuoteDetail} path="/quotes/:quoteId" />
-          <Route component={NewQuote} path="/new-quote" />
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
+        <Suspense
+          fallback={
+            <div className="centered">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/quotes" />
+            </Route>
+            <Route component={Quotes} path="/quotes" exact />
+            <Route component={QuoteDetail} path="/quotes/:quoteId" />
+            <Route component={NewQuote} path="/new-quote" />
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
     </BrowserRouter>
   );
