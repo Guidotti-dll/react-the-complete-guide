13562c30cc91acb232237819e639e3edcd67b4cd
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-01 19:07:28 -0300

    feat: finished pages

--- project-20/pages/[meetupId]/index.jsx
+++ project-20/pages/[meetupId]/index.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-
-const MeetupDetail = () => {
-  return <div>detail</div>;
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWBjyR556y3eO5lxknx7RChf7DQRIffDZvSqYwdkIfQa6pdT69wvrSfaCwfGJPPbDBqt4&usqp=CAU"
+      title="Mountains"
+      address="BC, canada"
+      description="a great mountain"
+    />
+  );
 };
 
-export default MeetupDetail;
+export default MeetupDetails;
