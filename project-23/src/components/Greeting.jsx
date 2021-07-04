d227f1348cb63c5f3a89a1c2770009c63d639c2a
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-04 15:49:15 -0300

    feat: test user interaction

--- project-23/src/components/Greeting.jsx
+++ project-23/src/components/Greeting.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello world!</h2>
-      <p>It's good see you!</p>
+      {!changedText && <p>It's good see you!</p>}
+      {changedText && <p>changed!</p>}
+      <button onClick={changeTextHandler}>Change Text</button>
     </div>
   );
 };
--- project-23/src/components/Greeting.test.js
+++ project-23/src/components/Greeting.test.js
@@ -1,12 +1,39 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
-test("renders Hello World as a text", () => {
-  //Arrenge
-  render(<Greeting />);
 
-  //Act
-  // ... nothing
-  //Assert
-  const helloWorldElement = screen.getByText("Hello world!");
-  expect(helloWorldElement).toBeInTheDocument();
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    //Arrenge
+    render(<Greeting />);
+
+    //Act
+    // ... nothing
+    //Assert
+    const helloWorldElement = screen.getByText("Hello world!");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+  test("renders 'good see you' if th button was NOT clicked", () => {
+    render(<Greeting />);
+    const paragraphElement = screen.getByText("It's good see you!", {
+      exact: false,
+    });
+    expect(paragraphElement).toBeInTheDocument();
+  });
+  test("renders 'changed!' if th button was clicked", () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    const paragraphElement = screen.getByText("changed!");
+    expect(paragraphElement).toBeInTheDocument();
+  });
+  test("does not renders 'good see you' if th button was clicked", () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    const paragraphElement = screen.queryByText("It's good see you!", {
+      exact: false,
+    });
+    expect(paragraphElement).toBeNull();
+  });
 });
