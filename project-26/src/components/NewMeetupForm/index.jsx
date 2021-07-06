6360bdc0d9ed629bd1b70120e48604f2b095c34b
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-06 15:04:20 -0300

    feat: add newMeetupform

---
+++ project-26/src/components/NewMeetupForm/index.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import styles from "./styles.module.css";
+import Card from "../Card";
+
+const NewMeetupForm = () => {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const submitHandler = (event) => {
+    const newMeetup = {
+      title: titleInputRef.current.value,
+      image: imageInputRef.current.value,
+      address: addressInputRef.current.value,
+      description: descriptionInputRef.current.value,
+    };
+
+    console.log(newMeetup);
+    event.preventDefault();
+  };
+
+  return (
+    <Card>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <div className={styles.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" id="title" required ref={titleInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" id="image" required ref={imageInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" required ref={addressInputRef} />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            rows="5"
+            required
+            ref={descriptionInputRef}
+          ></textarea>
+        </div>
+        <div className={styles.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default NewMeetupForm;
--- project-26/src/pages/NewMeetup/index.jsx
+++ project-26/src/pages/NewMeetup/index.jsx
@@ -1,5 +1,11 @@
+import NewMeetupForm from "../../components/NewMeetupForm";
 const NewMeetupPage = () => {
-  return <div>New Meetups page</div>;
+  return (
+    <section>
+      <h1>Add New Meetups</h1>
+      <NewMeetupForm />
+    </section>
+  );
 };
 
 export default NewMeetupPage;
