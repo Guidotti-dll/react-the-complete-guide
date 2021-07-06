4632dd3de9e4d7d1209d31f68d0f73fe6c30a17b
Guidotti-dll <dev.lucasguidotti@gmail.com>
2021-07-06 14:40:27 -0300

    feat: add Card and Layout components

--- project-26/src/components/MeetupItem/index.jsx
+++ project-26/src/components/MeetupItem/index.jsx
@@ -1,16 +1,19 @@
+import Card from "../Card";
 import styles from "./styles.module.css";
 const MeetupItem = ({ image, title, address, description }) => {
   return (
     <li className={styles.item}>
-      <img className={styles.image} src={image} alt={title} />
-      <div className={styles.content}>
-        <h3>{title}</h3>
-        <address>{address}</address>
-        <p>{description}</p>
-      </div>
-      <div className={styles.actions}>
-        <button>To favorite</button>
-      </div>
+      <Card>
+        <img className={styles.image} src={image} alt={title} />
+        <div className={styles.content}>
+          <h3>{title}</h3>
+          <address>{address}</address>
+          <p>{description}</p>
+        </div>
+        <div className={styles.actions}>
+          <button>To favorite</button>
+        </div>
+      </Card>
     </li>
   );
 };
--- project-26/src/routes.jsx
+++ project-26/src/routes.jsx
@@ -2,17 +2,18 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetup";
-import MainNavigation from "./components/MainNavigation";
+import Layout from "./components/Layout";
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <MainNavigation />
-      <Switch>
-        <Route path="/" exact component={AllMeetupsPage} />
-        <Route path="/new-meetup" component={NewMeetupsPage} />
-        <Route path="/favorites" component={FavoritesPage} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={AllMeetupsPage} />
+          <Route path="/new-meetup" component={NewMeetupsPage} />
+          <Route path="/favorites" component={FavoritesPage} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 };
